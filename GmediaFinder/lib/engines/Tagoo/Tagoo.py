a7ad4f95f7d517c59fac9cb9a4b04b5acfbd1d00
smo <smo@smo-pc.(none)>
2011-06-12 11:52:36 +0200

    fix

--- GmediaFinder/lib/engines/Tagoo/Tagoo.py
+++ GmediaFinder/lib/engines/Tagoo/Tagoo.py
@@ -58,7 +58,7 @@ class Tagoo(object):
                 gobject.idle_add(self.gui.pageback_btn.hide)
         else:
             gobject.idle_add(self.gui.changepage_btn.hide)
-            self.print_info(_("mp3Realm: no results found for %s...") % user_search)
+            self.print_info(_("Tagoo: no results found for %s...") % user_search)
             gobject.idle_add(self.gui.throbber.hide)
             time.sleep(5)
             self.print_info('')
